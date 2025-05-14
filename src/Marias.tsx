3d7e00ac84d64e11d45b2438f5ea8a10d90ba666
Feliziaboustedt <felizia.boustedt@hotmail.com>
2025-05-14 11:54:30 +0200

    nu försvinner allt säkert :)

--- src/Marias.tsx
+++ src/Marias.tsx
@@ -1,6 +1,7 @@
 import { DigiLayoutBlock, DigiLayoutContainer, DigiTypography } from "@digi/arbetsformedlingen-react";
+
 export const Marias = () => {
   return <>
-  <DigiLayoutBlock 
+  <DigiLayoutBlock></DigiLayoutBlock>
   </>;
 };
