5cdc8f2834fc578e4a0dcecd3bd48b04d5d9a680
MariaHellsen <maria.hellsen@outlook.com>
2025-05-14 11:52:00 +0200

    Add changes in Marias file

--- src/Marias.tsx
+++ src/Marias.tsx
@@ -1,6 +1,19 @@
-import { DigiLayoutBlock, DigiLayoutContainer, DigiTypography } from "@digi/arbetsformedlingen-react";
+import {
+  DigiLayoutBlock,
+  DigiLayoutContainer,
+  DigiTypography,
+  DigiCalender,
+} from "@digi/arbetsformedlingen-react";
 export const Marias = () => {
-  return <>
-  <DigiLayoutBlock 
-  </>;
+  return (
+    <>
+      <DigiLayoutBlock>
+        <DigiTypography>
+          <DigiLayoutContainer>
+            <DigiCalender afActive={true} afMultipleDates={true}></DigiCalender>
+          </DigiLayoutContainer>
+        </DigiTypography>
+      </DigiLayoutBlock>
+    </>
+  );
 };
