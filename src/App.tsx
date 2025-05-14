2b4848dabf6107019538c32b100c35831f77c64d
MariaHellsen <maria.hellsen@outlook.com>
2025-05-14 12:14:24 +0200

    Added calendar to div3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import {
   DigiLayoutBlock,
   DigiLayoutContainer,
   DigiTypography,
+  DigiCalendar,
+  DigiButton,
 } from "@digi/arbetsformedlingen-react";
 import "./App.css";
 import { LayoutBlockVariation } from "@digi/arbetsformedlingen";
@@ -18,22 +20,30 @@ function App() {
         <DigiTypography>
           <DigiLayoutContainer>
             <div></div>
-            <div><DigiInfoCard
-	afHeading="Jag är ett infokort"
-	afHeadingLevel={InfoCardHeadingLevel.H2}
-	afType={InfoCardType.TIP}
-	afLinkHref="Frivillig länk"	
- 	afLinkText="Frivillig länktext"	
- 	afVariation={InfoCardVariation.PRIMARY}	
- 	afSize={InfoCardSize.STANDARD}
->
-	<p>
-		Det här är bara ord för att illustrera hur det ser ut med text inuti. Lorem ipsum dolor sit amet,
-		consectetur adipiscing elit. Suspendisse commodo egestas elit in consequat. Proin in ex consectetur,
-		laoreet augue sit amet, malesuada tellus.
-	</p>
-</DigiInfoCard></div>
-            <div></div>
+            <div>
+              <DigiInfoCard
+                afHeading="Jag är ett infokort"
+                afHeadingLevel={InfoCardHeadingLevel.H2}
+                afType={InfoCardType.TIP}
+                afLinkHref="Frivillig länk"
+                afLinkText="Frivillig länktext"
+                afVariation={InfoCardVariation.PRIMARY}
+                afSize={InfoCardSize.STANDARD}
+              >
+                <p>
+                  Det här är bara ord för att illustrera hur det ser ut med text
+                  inuti. Lorem ipsum dolor sit amet, consectetur adipiscing
+                  elit. Suspendisse commodo egestas elit in consequat. Proin in
+                  ex consectetur, laoreet augue sit amet, malesuada tellus.
+                </p>
+              </DigiInfoCard>
+            </div>
+            <div>
+              {" "}
+              <DigiCalendar afActive={true} afMultipleDates={true}>
+                <DigiButton slot="calendar-footer">Välj</DigiButton>
+              </DigiCalendar>
+            </div>
           </DigiLayoutContainer>
         </DigiTypography>
       </DigiLayoutBlock>
