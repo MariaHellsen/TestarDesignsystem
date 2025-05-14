2cb802aaab488044945541a09d0aa4001e7d80f4
Feliziaboustedt <felizia.boustedt@hotmail.com>
2025-05-14 11:07:55 +0200

    added info in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
-
+import { DigiLayoutBlock, DigiLayoutContainer, DigiTypography } from "@digi/arbetsformedlingen-react";
 import "./App.css";
+import {  LayoutBlockVariation } from "@digi/arbetsformedlingen";
 
 function App() {
-  return <></>;
+  return <>
+  <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
+  <DigiTypography>
+
+  <DigiLayoutContainer> 
+    <div></div>
+    <div></div>
+    <div></div>
+    </DigiLayoutContainer>
+  </DigiTypography>
+  </DigiLayoutBlock>
+  </>;
 }
 
 export default App;
